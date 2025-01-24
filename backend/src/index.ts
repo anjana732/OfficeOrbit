880d56cd1eba42cbd5749129f0c198ef15935530
anjana732 <anjanasingh1257@gmail.com>
2025-01-24 12:13:24 +0530

    Configure app.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,19 @@
 import dotenv from "dotenv"
 import mongoose from "mongoose";
 import connectDB from "./db";
+import { app } from "./app";
 
 dotenv.config({
     path: './env'
 })
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    })
+})
+.catch(()=>{
+    console.log("DB connection failed")
+});
