4f3f156bf5ba5dac631fc54d7175b8e4b78d9432
anjana732 <anjanasingh1257@gmail.com>
2025-01-25 17:39:16 +0530

    Add middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,16 @@
-import express from "express";
+import express, { urlencoded } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+import { url } from "inspector";
+import exp from "constants";
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN
+}));
+app.use(cookieParser());
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({extended: true , limit: "16kb"}));
+app.use(express.static("public"))
 export {app}
